f11b7dcaf7a0772e616341644b729ce4fb931598
Lachs <ttkaykk@gmail.com>
2026-08-15 22:50:17 +0200

    Initial Commit

---
+++ money./money_App.swift
@@ -0,0 +1,17 @@
+//
+//  money_App.swift
+//  money.
+//
+//  Created by Till Kokemoor on 15.08.26.
+//
+
+import SwiftUI
+
+@main
+struct money_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
